c36f0b81cbbb50555740b58460ed7e9ceefe386d
pderyuga <polina.deryuga@persuit.com>
2025-08-12 12:36:43 -0400

    add base functionality

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -10,13 +17,45 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
         screen.fill('#000000')
+        
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.has_collided(player):
+                print('Game over!')
+                sys.exit()
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.has_collided(asteroid):
+                    asteroid.split()
+                    shot.kill()
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
